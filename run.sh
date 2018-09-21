83a127add61b9e72c63f064f08da9fb3bec0e89c
SharafutdinovRuslan <sharafutdinov@phystech.edu>
2018-09-21 13:14:15 +0300

    add FirstEnv

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-
+virtualenv FirstEnv
 source ./FirstEnv/bin/activate 
-pip install -r FirstEnv/bin/requirements_dev.txt
-jupyter nbconvert —execute Ex_1.ipynb
+FirstEnv/bin/pip install -r requirements_dev.txt
+jupyter nbconvert --execute Ex_1.ipynb
 open Ex_1.html
 deactivate
